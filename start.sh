c3c78610b0062c81daadb8090cee12ec7e13a88c
blade1981m <blade1981m@gmail.com>
2017-03-01 20:43:33 -0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-cd /starbound/linux
+/steamcmd/steamcmd.sh +@ShutdownOnFailedCommand 1 +@NoPromptForPassword 1 +set_steam_guard_code $STEAMGUARD_CODE +login $STEAM_USERNAME $STEAM_PASSWORD +force_install_dir /starbound/ +app_update "211820" +quit
+
+cd /starbound/linux64
 
 ./starbound_server
